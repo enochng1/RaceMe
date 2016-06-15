8ba2394e49eed8ab22653aa8ade593d263e90e70
enochng1 <enochng1@gmail.com>
2016-06-14 22:07:32 -0700

    updated and experimented with tilting and all that

--- TrainViewController.swift
+++ TrainViewController.swift
@@ -8,31 +8,129 @@
 
 import UIKit
 import Mapbox
+import CoreLocation
 
-class TrainViewController: UIViewController,  MGLMapViewDelegate {
-
+class TrainViewController: UIViewController,  MGLMapViewDelegate, CLLocationManagerDelegate {
+    
     @IBOutlet weak var mapView: MGLMapView!
+    
+    var tracking:Bool = (false)
+    var trackCamera : MGLMapCamera!
+    var staticCamera : MGLMapCamera!
+    
+    var currentLocation : CLLocation!
+    var trackedLocations : [CLLocation]!
+    
+    lazy var locationManager: CLLocationManager! = {
+        let manager = CLLocationManager()
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.delegate = self
+//        manager.distanceFilter = 1.0
+        manager.requestAlwaysAuthorization()
+        return manager
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        mapView.delegate = self
+        
+        locationManager.delegate = self
+        
+        if(CLLocationManager.authorizationStatus() == .NotDetermined){
+            locationManager.requestAlwaysAuthorization()
+        }
+        
+        mapView.showsUserLocation = true
+        mapView.zoomLevel = 17
+        
+        trackedLocations = [CLLocation]()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if(status == .AuthorizedAlways){
+            locationManager.startUpdatingLocation()
+        }
     }
-    */
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
 
+        currentLocation = locations.last
+        mapView.setCenterCoordinate(currentLocation.coordinate, animated: true)
+        
+        if UIApplication.sharedApplication().applicationState == .Active {
+           // print(currentLocation.coordinate)
+        } else {
+           // print(currentLocation.coordinate)
+        }
+        
+        if(tracking){
+            trackedLocations.append(currentLocation)
+            print(trackedLocations)
+        }
+        
+    }
+    
+    //    func mapView(mapView: MGLMapView, didUpdateUserLocation userLocation: MGLUserLocation?){
+    ////
+    //        if(!tracking){
+    //
+    //            //            staticCamera = MGLMapCamera(lookingAtCenterCoordinate: self.mapView.userLocation!.location!.coordinate, fromDistance: 500, pitch: 0, heading: 0)
+    //            //            mapView.setCamera(staticCamera, animated: true)
+    //            mapView.setCenterCoordinate(userLocation!.coordinate, animated: true)
+    //        } else {
+    //
+    //        }
+    //        mapView.userTrackingMode = .FollowWithHeading
+    //        if UIApplication.sharedApplication().applicationState == .Active {
+    //            print(userLocation)
+    //        } else {
+    //            print(userLocation)
+    //        }
+    //
+    //
+    //    }
+    
+    
+    @IBAction func startTracking(sender: UIButton) {
+        
+        if(tracking){
+            tracking = false
+            print("end of run")
+            
+            
+            
+            
+            
+            
+//            staticCamera = MGLMapCamera(lookingAtCenterCoordinate:currentLocation.coordinate, fromDistance: 100, pitch: 0, heading: 0)
+//            mapView.setCamera(staticCamera, animated: true)
+//            print(mapView.camera)
+//            mapView.zoomLevel = 18
+            
+            
+            
+        } else {
+            tracking = true
+            print("starting run")
+            
+        
+//            trackCamera = MGLMapCamera(lookingAtCenterCoordinate:currentLocation.coordinate, fromDistance: 100, pitch: 60, heading: 0)
+//             mapView.setCamera(trackCamera, animated: true)
+//              print(mapView.camera)
+  
+            //            print(self.mapView.userLocation!.heading?.trueHeading)
+            //self.mapView.userTrackingMode = .FollowWithHeading
+            //            print(self.mapView.userLocation!.heading)
+            
+            //            trackCamera = MGLMapCamera(lookingAtCenterCoordinate: self.mapView.userLocation!.location!.coordinate, fromDistance: 100, pitch: 80, heading: 0)
+            //            mapView.setCamera(trackCamera, animated: true)
+            
+            //            mapView.userTrackingMode = .FollowWithHeading
+            //            mapView.camera.pitch = 80;
+            
+        }
+        
+    }
+    
+    
 }
